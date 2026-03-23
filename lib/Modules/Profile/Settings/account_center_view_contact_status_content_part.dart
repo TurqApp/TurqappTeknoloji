2ab96e2714778a1981008b80c15e47f6eeaf49ff
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-23 23:55:02 +0300

    Extract shared account center info content

--- lib/Modules/Profile/Settings/account_center_view_contact_status_content_part.dart
+++ lib/Modules/Profile/Settings/account_center_view_contact_status_content_part.dart
@@ -2,13 +2,9 @@ part of 'account_center_view.dart';
 
 extension AccountCenterViewContactStatusContentPart on _ContactStatusRow {
   Widget _buildContactStatusContent() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildContactStatusTitle(),
-        const SizedBox(height: 3),
-        _buildContactStatusValue(),
-      ],
+    return _buildAccountCenterInfoContent(
+      title: title,
+      value: value,
     );
   }
 }
--- lib/Modules/Profile/Settings/account_center_view_personal_row_content_part.dart
+++ lib/Modules/Profile/Settings/account_center_view_personal_row_content_part.dart
@@ -2,13 +2,9 @@ part of 'account_center_view.dart';
 
 extension AccountCenterViewPersonalRowContentPart on _PersonalDetailRow {
   Widget _buildPersonalRowContent() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildPersonalRowTitle(),
-        const SizedBox(height: 3),
-        _buildPersonalRowValue(),
-      ],
+    return _buildAccountCenterInfoContent(
+      title: title,
+      value: value,
     );
   }
 }
