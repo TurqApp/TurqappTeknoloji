05b63ee07094cb5801ceb3e68b9c387a2e189014
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 02:38:51 +0300

    Merge account center personal section into body content

--- lib/Modules/Profile/Settings/account_center_view.dart
+++ lib/Modules/Profile/Settings/account_center_view.dart
@@ -25,7 +25,6 @@ part 'account_center_view_card_shell_part.dart';
 part 'account_center_view_chevron_part.dart';
 part 'account_center_view_row_shell_part.dart';
 part 'account_center_view_info_content_part.dart';
-part 'account_center_view_personal_section_part.dart';
 
 class AccountCenterView extends StatelessWidget {
   AccountCenterView({super.key});
