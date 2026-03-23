cc08cf138c11e275bacba439b7390c491cba0079
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 02:21:50 +0300

    Merge account center remove flow into body content

--- lib/Modules/Profile/Settings/account_center_view.dart
+++ lib/Modules/Profile/Settings/account_center_view.dart
@@ -34,7 +34,6 @@ part 'account_center_view_personal_data_direct_part.dart';
 part 'account_center_view_personal_data_fallback_part.dart';
 part 'account_center_view_personal_row_part.dart';
 part 'account_center_view_personal_section_part.dart';
-part 'account_center_view_remove_part.dart';
 
 class AccountCenterView extends StatelessWidget {
   AccountCenterView({super.key});
