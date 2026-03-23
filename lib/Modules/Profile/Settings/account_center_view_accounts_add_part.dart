a9d2862c93321077ed14af0b266acb488a279e58
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-23 23:09:33 +0300

    Split account center add account body into part

--- lib/Modules/Profile/Settings/account_center_view_accounts_add_part.dart
+++ lib/Modules/Profile/Settings/account_center_view_accounts_add_part.dart
@@ -7,20 +7,7 @@ extension AccountCenterViewAccountsAddPart on AccountCenterView {
         bottom: Radius.circular(18),
       ),
       onTap: _handleAddAccountTap,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 18,
-          vertical: 18,
-        ),
-        child: Text(
-          'account_center.add_account'.tr,
-          style: const TextStyle(
-            color: Color(0xFF3797EF),
-            fontSize: 15,
-            fontFamily: 'MontserratBold',
-          ),
-        ),
-      ),
+      child: _buildAddAccountActionBody(),
     );
   }
 }
