4d26ca7b89b57d1a0792883a4c565d63d28f45f5
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 01:27:22 +0300

    Inline account center add account body

--- lib/Modules/Profile/Settings/account_center_view_accounts_add_part.dart
+++ lib/Modules/Profile/Settings/account_center_view_accounts_add_part.dart
@@ -7,7 +7,20 @@ extension AccountCenterViewAccountsAddPart on AccountCenterView {
         bottom: Radius.circular(18),
       ),
       onTap: () => Get.to(() => SignIn()),
-      child: _buildAddAccountActionBody(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18,
+          vertical: 18,
+        ),
+        child: Text(
+          'account_center.add_account'.tr,
+          style: const TextStyle(
+            color: Color(0xFF3797EF),
+            fontSize: 15,
+            fontFamily: 'MontserratBold',
+          ),
+        ),
+      ),
     );
   }
 }
