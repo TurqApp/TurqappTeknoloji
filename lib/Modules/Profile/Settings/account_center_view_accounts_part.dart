0d86cd8335a0e5594102862bcff1c92e53d4f034
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-23 19:33:38 +0300

    Split account center accounts card into part

--- lib/Modules/Profile/Settings/account_center_view_accounts_part.dart
+++ lib/Modules/Profile/Settings/account_center_view_accounts_part.dart
@@ -9,69 +9,7 @@ extension AccountCenterViewAccountsPart on AccountCenterView {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         _buildAccountsHeader(),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(18),
-            border: Border.all(color: Colors.black12),
-          ),
-          child: items.isEmpty
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 18,
-                    vertical: 22,
-                  ),
-                  child: Text(
-                    'account_center.no_accounts'.tr,
-                    style: const TextStyle(
-                      color: Colors.black54,
-                      fontSize: 14,
-                      fontFamily: 'MontserratMedium',
-                    ),
-                  ),
-                )
-              : Column(
-                  children: [
-                    for (var i = 0; i < items.length; i++) ...[
-                      _AccountRow(
-                        account: items[i],
-                        avatar: _avatar(items[i]),
-                        onTap: () => _continueWithAccount(items[i]),
-                        onLongPress: () => _confirmRemoveAccount(
-                          context,
-                          items[i],
-                        ),
-                      ),
-                      if (i != items.length - 1)
-                        const Divider(
-                          height: 1,
-                          indent: 84,
-                          endIndent: 16,
-                        ),
-                    ],
-                    InkWell(
-                      borderRadius: const BorderRadius.vertical(
-                        bottom: Radius.circular(18),
-                      ),
-                      onTap: () => Get.to(() => SignIn()),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 18,
-                          vertical: 18,
-                        ),
-                        child: Text(
-                          'account_center.add_account'.tr,
-                          style: const TextStyle(
-                            color: Color(0xFF3797EF),
-                            fontSize: 15,
-                            fontFamily: 'MontserratBold',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-        ),
+        _buildAccountsCard(context, items),
       ],
     );
   }
