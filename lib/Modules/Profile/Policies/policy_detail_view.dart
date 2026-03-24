27ab3015cdece68c962c298d947206e0bd2772d9
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 06:29:34 +0300

    Merge policy detail shell into main view

--- lib/Modules/Profile/Policies/policy_detail_view.dart
+++ lib/Modules/Profile/Policies/policy_detail_view.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:turqappv2/Core/Buttons/back_buttons.dart';
 import 'package:turqappv2/Modules/Profile/Policies/policy_content.dart';
 
-part 'policy_detail_view_shell_part.dart';
 part 'policy_detail_view_content_part.dart';
 
 class PolicyDetailView extends StatelessWidget {
@@ -13,6 +12,20 @@ class PolicyDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _buildPolicyDetailShell(context);
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6F7FB),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+              child: BackButtons(text: policy.title),
+            ),
+            Expanded(child: _buildPolicyDetailContent()),
+          ],
+        ),
+      ),
+    );
   }
 }
