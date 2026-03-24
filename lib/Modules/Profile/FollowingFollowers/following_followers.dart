b65a15702ec481f3d644a611fb47ed3a2cfd5ea9
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 06:39:20 +0300

    Merge following followers shell into main view

--- lib/Modules/Profile/FollowingFollowers/following_followers.dart
+++ lib/Modules/Profile/FollowingFollowers/following_followers.dart
@@ -8,7 +8,6 @@ import 'package:turqappv2/Core/page_line_bar.dart';
 import 'package:turqappv2/Modules/Profile/FollowingFollowers/follower_content.dart';
 import 'package:turqappv2/Modules/Profile/FollowingFollowers/following_followers_controller.dart';
 
-part 'following_followers_shell_part.dart';
 part 'following_followers_content_part.dart';
 
 class FollowingFollowers extends StatefulWidget {
@@ -73,7 +72,74 @@ class _FollowingFollowersState extends State<FollowingFollowers> {
 
   @override
   Widget build(BuildContext context) {
-    return _buildFollowingFollowersShell(context);
+    return Scaffold(
+      key: const ValueKey(IntegrationTestKeys.screenFollowingFollowers),
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Obx(
+                  () => BackButtons(text: controller.nickname.value),
+                ),
+                PageLineBar(
+                  barList: [
+                    'following.followers_tab'.tr,
+                    'following.following_tab'.tr,
+                  ],
+                  pageName: _pageLineBarTag,
+                  initialIndex: widget.selection,
+                  pageController: controller.pageController,
+                ),
+                Expanded(
+                  child: PageView(
+                    controller: controller.pageController,
+                    onPageChanged: (idx) {
+                      _setCurrentPage(idx);
+                      syncPageLineBarSelection(_pageLineBarTag, idx);
+                    },
+                    children: [
+                      _buildFollowersList(
+                        list: controller.takipciler,
+                        isLoading: () => controller.isLoadingFollowers,
+                        hasMore: () => controller.hasMoreFollowers,
+                        scrollController: _followersScrollController,
+                        loadMore: () => controller.getFollowers(initial: false),
+                      ),
+                      _buildFollowersList(
+                        list: controller.takipEdilenler,
+                        isLoading: () => controller.isLoadingFollowing,
+                        hasMore: () => controller.hasMoreFollowing,
+                        scrollController: _followingScrollController,
+                        loadMore: () => controller.getFollowing(initial: false),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: GestureDetector(
+                onTap: () {
+                  final activeController = _currentPage == 0
+                      ? _followersScrollController
+                      : _followingScrollController;
+                  activeController.animateTo(
+                    0,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.bounceIn,
+                  );
+                },
+                child: RoadToTop(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   void _setCurrentPage(int index) {
