d7c8ec8f776e540a57bea7b32b40fb473bd431bc
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 04:32:03 +0300

    Merge my admin approval results shell into main view

--- lib/Modules/Profile/Settings/my_admin_approval_results_view.dart
+++ lib/Modules/Profile/Settings/my_admin_approval_results_view.dart
@@ -5,7 +5,6 @@ import 'package:turqappv2/Core/Buttons/back_buttons.dart';
 import 'package:turqappv2/Core/Repositories/admin_approval_repository.dart';
 import 'package:turqappv2/Services/current_user_service.dart';
 
-part 'my_admin_approval_results_view_shell_part.dart';
 part 'my_admin_approval_results_view_content_part.dart';
 part 'my_admin_approval_results_view_format_part.dart';
 part 'my_admin_approval_results_view_query_part.dart';
@@ -18,6 +17,24 @@ class MyAdminApprovalResultsView extends StatelessWidget {
   const MyAdminApprovalResultsView({super.key});
 
   String get _currentUid => CurrentUserService.instance.effectiveUserId;
+
+  Widget _buildPage(BuildContext context) {
+    final uid = _currentUid;
+
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            BackButtons(text: 'admin.my_approvals.title'.tr),
+            Expanded(
+              child: _buildApprovalStream(uid),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) => _buildPage(context);
