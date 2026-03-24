77e8c4b6b3ed041a740c444f3b9940a7ff0b58b1
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 09:30:15 +0300

    Merge exam results preview controller helpers into main controller

--- lib/Modules/Education/PracticeExams/SinavSonuclariPreview/sinav_sonuclari_preview_controller_actions_part.dart
+++
@@ -1,8 +0,0 @@
-part of 'sinav_sonuclari_preview_controller.dart';
-
-extension SinavSonuclariPreviewControllerActionsPart
-    on SinavSonuclariPreviewController {
-  void _toggleCategoryImpl(String ders) {
-    expandedCategories[ders] = !expandedCategories[ders]!;
-  }
-}
