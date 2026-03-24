d4f5559e2dadb756449af4474fe1a75efed7c850
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 08:52:13 +0300

    Merge my tests view parts into main view

--- lib/Modules/Education/Tests/MyTests/my_tests.dart
+++ lib/Modules/Education/Tests/MyTests/my_tests.dart
@@ -6,9 +6,6 @@ import 'package:turqappv2/Modules/Education/Tests/CreateTest/create_test.dart';
 import 'package:turqappv2/Modules/Education/Tests/MyTests/my_tests_controller.dart';
 import 'package:turqappv2/Modules/Education/Tests/TestsGrid/tests_grid.dart';
 
-part 'my_tests_shell_part.dart';
-part 'my_tests_content_part.dart';
-
 class MyTests extends StatefulWidget {
   const MyTests({super.key});
 
@@ -46,5 +43,137 @@ class _MyTestsState extends State<MyTests> {
   @override
   Widget build(BuildContext context) {
     return _buildPage();
+  }
+
+  Widget _buildPage() {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            Column(
+              children: [
+                BackButtons(text: 'tests.my_tests_title'.tr),
+                Expanded(
+                  child: RefreshIndicator(
+                    color: Colors.white,
+                    backgroundColor: Colors.black,
+                    onRefresh: controller.getData,
+                    child: Obx(() => _buildContent()),
+                  ),
+                ),
+              ],
+            ),
+            _buildCreateButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCreateButton() {
+    return GestureDetector(
+      onTap: () => Get.to(() => const CreateTest()),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Obx(
+              () => controller.list.isEmpty
+                  ? Transform.translate(
+                      offset: const Offset(-20, -30),
+                      child: Image.asset(
+                        'assets/education/arrowdef.webp',
+                        height: 80,
+                      ),
+                    )
+                  : const SizedBox.shrink(),
+            ),
+            Container(
+              height: 60,
+              width: 60,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(12),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    blurRadius: 20,
+                    spreadRadius: 6,
+                  ),
+                ],
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    if (controller.isLoading.value) {
+      return const Center(
+        child: CupertinoActivityIndicator(
+          radius: 20,
+          color: Colors.black,
+        ),
+      );
+    }
+
+    if (controller.list.isEmpty) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.info_outline,
+              color: Colors.black,
+              size: 40,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'tests.my_tests_empty'.tr,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontFamily: 'Montserrat',
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 15),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 5.0,
+          childAspectRatio: 1.85 / 3.6,
+        ),
+        itemCount: controller.list.length,
+        itemBuilder: (context, index) {
+          return TestsGrid(
+            model: controller.list[index],
+            update: controller.getData,
+          );
+        },
+      ),
+    );
   }
 }
