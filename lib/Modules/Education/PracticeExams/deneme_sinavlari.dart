7638c6909e6e03386fe3bce3b0f18168a0537f3b
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 10:27:36 +0300

    Merge practice exams shell into main view

--- lib/Modules/Education/PracticeExams/deneme_sinavlari.dart
+++ lib/Modules/Education/PracticeExams/deneme_sinavlari.dart
@@ -27,7 +27,6 @@ import 'package:turqappv2/Utils/empty_padding.dart';
 part 'deneme_sinavlari_content_part.dart';
 part 'deneme_sinavlari_actions_part.dart';
 part 'deneme_sinavlari_sections_part.dart';
-part 'deneme_sinavlari_shell_part.dart';
 
 class DenemeSinavlari extends StatelessWidget {
   DenemeSinavlari({
@@ -44,4 +43,81 @@ class DenemeSinavlari extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => _buildPage(context);
+
+  Widget _buildPage(BuildContext context) {
+    final bodyContent = _buildBodyContent(context);
+
+    if (embedded) {
+      return Stack(
+        children: [
+          Column(children: [bodyContent]),
+          Obx(
+            () => controller.showOkulAlert.value
+                ? _buildSchoolAlertSheet(context)
+                : const SizedBox.shrink(),
+          ),
+          if (showEmbeddedControls)
+            ScrollTotopButton(
+              scrollController: _scrollController,
+              visibilityThreshold: 350,
+            ),
+          if (showEmbeddedControls) _buildFloatingAction(context),
+        ],
+      );
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                _buildHeader(),
+                bodyContent,
+              ],
+            ),
+            Obx(
+              () => controller.showOkulAlert.value
+                  ? _buildSchoolAlertSheet(context)
+                  : const SizedBox.shrink(),
+            ),
+            ScrollTotopButton(
+              scrollController: _scrollController,
+              visibilityThreshold: 350,
+            ),
+            _buildFloatingAction(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Row(
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: Get.back,
+                icon: const Icon(
+                  AppIcons.arrowLeft,
+                  color: Colors.black,
+                  size: 25,
+                ),
+              ),
+              TypewriterText(
+                text: 'pasaj.tabs.online_exam'.tr,
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          onPressed: () => Get.to(() => SearchDeneme()),
+          icon: const Icon(AppIcons.search, color: Colors.black),
+        ),
+      ],
+    );
+  }
 }
