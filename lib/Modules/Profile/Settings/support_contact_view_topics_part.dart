6e585f4cac6ce909f7605936a4593926eed71e57
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 03:17:57 +0300

    Merge support contact topics catalog into topics

--- lib/Modules/Profile/Settings/support_contact_view_topics_part.dart
+++ lib/Modules/Profile/Settings/support_contact_view_topics_part.dart
@@ -37,3 +37,11 @@ extension SupportContactViewTopicsPart on _SupportContactViewState {
     );
   }
 }
+
+const List<String> _supportTopicKeys = <String>[
+  'support.topic.account',
+  'support.topic.payment',
+  'support.topic.technical',
+  'support.topic.content',
+  'support.topic.suggestion',
+];
