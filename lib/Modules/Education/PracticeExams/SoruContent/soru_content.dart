e706385edd22db8326c2a4078c3fe02286f4b903
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 11:13:14 +0300

    Split question content actions into separate part

--- lib/Modules/Education/PracticeExams/SoruContent/soru_content.dart
+++ lib/Modules/Education/PracticeExams/SoruContent/soru_content.dart
@@ -12,6 +12,8 @@ import 'package:turqappv2/Core/Services/webp_upload_service.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/konu_model.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/soru_model.dart';
 
+part 'soru_content_actions_part.dart';
+
 const _practiceQuestionLgsType = 'LGS';
 
 class SoruContent extends StatefulWidget {
@@ -221,77 +223,5 @@ class _SoruContentState extends State<SoruContent> {
         ),
       ),
     );
-  }
-
-  Future<void> _pickImageFromGallery() async {
-    final ctx = Get.context;
-    if (ctx == null) return;
-    final pickedFile = await AppImagePickerService.pickSingleImage(ctx);
-    if (pickedFile != null) {
-      setState(() {
-        selectedImage = pickedFile;
-        yukle(selectedImage!, widget.mainID);
-      });
-    }
-  }
-
-  Future<void> _pickImageFromCamera() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
-    if (pickedFile != null) {
-      setState(() {
-        selectedImage = File(pickedFile.path);
-      });
-    }
-  }
-
-  Future<void> yukle(File imageFile, String mainID) async {
-    try {
-      final nsfw = await OptimizedNSFWService.checkImage(imageFile);
-      if (nsfw.errorMessage != null) {
-        AppSnackbar('common.error'.tr, 'tests.image_analyze_failed'.tr);
-        return;
-      }
-      if (nsfw.isNSFW) {
-        AppSnackbar('common.error'.tr, 'tests.image_invalid'.tr);
-        return;
-      }
-      final downloadUrl = await WebpUploadService.uploadFileAsWebp(
-        storage: FirebaseStorage.instance,
-        file: imageFile,
-        storagePathWithoutExt:
-            'practiceExams/$mainID/questions/${widget.model.docID}',
-      );
-
-      FirebaseFirestore.instance
-          .collection("practiceExams")
-          .doc(widget.mainID)
-          .collection("Sorular")
-          .doc(widget.model.docID)
-          .set({
-        "id": widget.model.id,
-        "soru": downloadUrl,
-        "ders": widget.ders,
-        "konu": konu,
-        "dogruCevap": dogruCevap,
-        "yanitlayanlar": [],
-      }, SetOptions(merge: true));
-    } catch (e) {
-      print("Hata oluştu: $e");
-    }
-  }
-
-  void fastSetData() {
-    FirebaseFirestore.instance
-        .collection("practiceExams")
-        .doc(widget.mainID)
-        .collection("Sorular")
-        .doc(widget.model.docID)
-        .set({
-      "id": widget.model.id,
-      "ders": widget.ders,
-      "konu": konu,
-      "dogruCevap": dogruCevap,
-      "yanitlayanlar": [],
-    }, SetOptions(merge: true));
   }
 }
