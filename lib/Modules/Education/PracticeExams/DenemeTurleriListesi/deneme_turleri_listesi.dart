9b63a82f47c1924799adb2645754a93b18560087
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 09:19:47 +0300

    Merge exam types view parts into main view

--- lib/Modules/Education/PracticeExams/DenemeTurleriListesi/deneme_turleri_listesi.dart
+++ lib/Modules/Education/PracticeExams/DenemeTurleriListesi/deneme_turleri_listesi.dart
@@ -5,9 +5,6 @@ import 'package:turqappv2/Core/Buttons/back_buttons.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/DenemeGrid/deneme_grid.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/DenemeTurleriListesi/deneme_turleri_listesi_controller.dart';
 
-part 'deneme_turleri_listesi_shell_part.dart';
-part 'deneme_turleri_listesi_content_part.dart';
-
 class DenemeTurleriListesi extends StatefulWidget {
   final String sinavTuru;
 
@@ -50,5 +47,86 @@ class _DenemeTurleriListesiState extends State<DenemeTurleriListesi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: _buildDenemeTurleriListesiBody());
+  }
+
+  Widget _buildDenemeTurleriListesiBody() {
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: [
+          BackButtons(text: widget.sinavTuru),
+          Expanded(child: _buildDenemeTurleriListesiContent()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDenemeTurleriListesiContent() {
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return const Center(
+          child: CupertinoActivityIndicator(radius: 20),
+        );
+      }
+
+      if (controller.isInitialized.value && controller.list.isEmpty) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.lightbulb_outline,
+                  color: Colors.black,
+                  size: 40,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'tests.not_found_in_type'
+                      .trParams({'type': widget.sinavTuru}),
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontFamily: "MontserratMedium",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+
+      return RefreshIndicator(
+        color: Colors.white,
+        backgroundColor: Colors.black,
+        onRefresh: controller.getData,
+        child: ListView(children: [_buildExamGrid()]),
+      );
+    });
+  }
+
+  Widget _buildExamGrid() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 5.0,
+          childAspectRatio: 2 / 4,
+        ),
+        itemCount: controller.list.length,
+        itemBuilder: (context, index) {
+          return DenemeGrid(
+            model: controller.list[index],
+            getData: controller.getData,
+          );
+        },
+      ),
+    );
   }
 }
