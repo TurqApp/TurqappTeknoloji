8f1d951ce7f9a9347d1cfbcfc0f17df0d22f4b62
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 06:36:08 +0300

    Merge my QR code controller data into main controller

--- lib/Modules/Profile/MyQRCode/my_q_r_code_controller.dart
+++ lib/Modules/Profile/MyQRCode/my_q_r_code_controller.dart
@@ -17,7 +17,6 @@ import 'package:turqappv2/Services/current_user_service.dart';
 
 import '../../../Core/Helpers/QRCode/qr_scanner_view.dart';
 
-part 'my_q_r_code_controller_data_part.dart';
 part 'my_q_r_code_controller_actions_part.dart';
 
 class MyQRCodeController extends GetxController {
@@ -43,6 +42,39 @@ class MyQRCodeController extends GetxController {
   final CurrentUserService userService = CurrentUserService.instance;
   final ShortLinkService _shortLinkService = ShortLinkService();
   final RxString profileLink = ''.obs;
+
+  String _buildProfileLink() {
+    final nickname = normalizeProfileSlug(userService.nickname);
+    if (nickname.isNotEmpty) {
+      return buildTurqAppProfileUrl(nickname);
+    }
+    final uid = userService.effectiveUserId;
+    if (uid.isNotEmpty) {
+      return buildTurqAppProfileUrl(uid);
+    }
+    return buildTurqAppProfileUrl('guest');
+  }
+
+  String _fallbackProfileLink() {
+    return _buildProfileLink();
+  }
+
+  Future<void> _prepareProfileLink() async {
+    final link = _buildProfileLink();
+    profileLink.value = link;
+    final uid = userService.effectiveUserId;
+    final nickname = normalizeProfileSlug(userService.nickname);
+    if (uid.isEmpty || nickname.isEmpty) return;
+    try {
+      await _shortLinkService.upsertUser(
+        userId: uid,
+        slug: nickname,
+        title: '@$nickname - TurqApp',
+        desc: 'qr.profile_desc'.tr,
+        imageUrl: userService.avatarUrl,
+      );
+    } catch (_) {}
+  }
 
   @override
   void onInit() {
