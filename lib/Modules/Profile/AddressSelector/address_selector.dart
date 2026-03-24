2e7db7a759ea42ff538a3367a32813a02e89703e
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 06:52:19 +0300

    Merge address selector content into main view

--- lib/Modules/Profile/AddressSelector/address_selector.dart
+++ lib/Modules/Profile/AddressSelector/address_selector.dart
@@ -4,8 +4,6 @@ import 'package:turqappv2/Core/Buttons/back_buttons.dart';
 import '../../../Core/Buttons/turq_app_button.dart';
 import 'address_selector_controller.dart';
 
-part 'address_selector_content_part.dart';
-
 class AddressSelector extends StatefulWidget {
   const AddressSelector({super.key});
 
@@ -59,6 +57,68 @@ class _AddressSelectorState extends State<AddressSelector> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Column(
+      children: [
+        Container(
+          constraints: const BoxConstraints(
+            minHeight: 150,
+            maxHeight: 150,
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+            color: Colors.black.withValues(alpha: 0.03),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: TextField(
+            controller: controller.addressController,
+            maxLength: 100,
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'address.hint'.tr,
+              hintStyle: const TextStyle(
+                color: Colors.grey,
+                fontFamily: "MontserratMedium",
+                fontSize: 15,
+              ),
+              counterText: "",
+            ),
+            style: const TextStyle(
+              fontSize: 15,
+              fontFamily: "MontserratMedium",
+              color: Colors.black,
+            ),
+          ),
+        ),
+        const SizedBox(height: 12),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Obx(() {
+              return Text(
+                "${controller.currentLength.value}/150",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontFamily: "MontserratMedium",
+                ),
+              );
+            }),
+          ],
+        ),
+        const SizedBox(height: 12),
+        TurqAppButton(
+          onTap: () {
+            controller.setData();
+          },
+        ),
+        const SizedBox(height: 12),
+      ],
     );
   }
 }
