95da6fc2fd58af4e2f43089ca85f0b592c01ab65
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 08:00:18 +0300

    Merge my applications controller actions into main controller

--- lib/Modules/JobFinder/MyApplications/my_applications_controller.dart
+++ lib/Modules/JobFinder/MyApplications/my_applications_controller.dart
@@ -8,7 +8,6 @@ import 'package:turqappv2/Models/job_application_model.dart';
 import 'package:turqappv2/Services/current_user_service.dart';
 
 part 'my_applications_controller_data_part.dart';
-part 'my_applications_controller_actions_part.dart';
 
 class MyApplicationsController extends GetxController {
   static MyApplicationsController ensure({
@@ -54,4 +53,30 @@ class MyApplicationsController extends GetxController {
 
   Future<void> cancelApplication(String jobDocID) =>
       _cancelApplicationImpl(jobDocID);
+
+  Future<void> _cancelApplicationImpl(String jobDocID) async {
+    try {
+      final uid = CurrentUserService.instance.effectiveUserId;
+      if (uid.isEmpty) return;
+
+      await _jobRepository.cancelApplication(
+        jobDocId: jobDocID,
+        userId: uid,
+      );
+
+      applications.removeWhere((a) => a.jobDocID == jobDocID);
+      await _subcollectionRepository.setEntries(
+        uid,
+        subcollection: 'myApplications',
+        items: applications
+            .map(
+              (e) => UserSubcollectionEntry(
+                id: e.jobDocID,
+                data: e.toMap(),
+              ),
+            )
+            .toList(growable: false),
+      );
+    } catch (_) {}
+  }
 }
