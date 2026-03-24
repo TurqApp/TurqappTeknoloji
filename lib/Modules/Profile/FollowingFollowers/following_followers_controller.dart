7a250c3ea9ff4fdf270a99e62d60c8213646a058
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 07:01:56 +0300

    Merge following followers controller lifecycle into main controller

--- lib/Modules/Profile/FollowingFollowers/following_followers_controller.dart
+++ lib/Modules/Profile/FollowingFollowers/following_followers_controller.dart
@@ -7,7 +7,6 @@ import 'package:turqappv2/Core/Utils/current_user_utils.dart';
 import 'package:turqappv2/Core/Utils/text_normalization_utils.dart';
 
 part 'following_followers_controller_cache_part.dart';
-part 'following_followers_controller_lifecycle_part.dart';
 part 'following_followers_controller_search_part.dart';
 
 class FollowingFollowersController extends GetxController {
@@ -177,6 +176,68 @@ class FollowingFollowersController extends GetxController {
         currentUid: currentUid,
         otherUserID: otherUserID,
         nowFollowing: nowFollowing,
+      );
+    }
+  }
+
+  void _handleOnInit() {
+    _loadNicknameCached();
+    getCounters();
+    final followersCached = _restoreRelationListCache(isFollowers: true);
+    final followingsCached = _restoreRelationListCache(isFollowers: false);
+    if (!followersCached) {
+      getFollowers(initial: true);
+    }
+    if (!followingsCached) {
+      getFollowing(initial: true);
+    }
+  }
+
+  void _handleOnClose() {
+    pageController.dispose();
+    searchTakipciController.dispose();
+    searchTakipEdilenController.dispose();
+  }
+
+  bool _resolveIsSelf() => isCurrentUserId(userId);
+
+  void _applyLocalMutation({
+    required String currentUid,
+    required String otherUserID,
+    required bool nowFollowing,
+  }) {
+    if (userId == currentUid) {
+      if (nowFollowing) {
+        if (!takipEdilenler.contains(otherUserID)) {
+          takipEdilenler.insert(0, otherUserID);
+        }
+      } else {
+        takipEdilenler.remove(otherUserID);
+      }
+      takipedilenCounter.value = nowFollowing
+          ? takipedilenCounter.value + 1
+          : (takipedilenCounter.value - 1).clamp(0, 1 << 30);
+      _saveRelationListCache(isFollowers: false);
+      _relationIdSetCache['followings'] = _RelationIdSetCacheEntry(
+        ids: takipEdilenler.toSet(),
+        cachedAt: DateTime.now(),
+      );
+    }
+    if (userId == otherUserID) {
+      if (nowFollowing) {
+        if (!takipciler.contains(currentUid)) {
+          takipciler.insert(0, currentUid);
+        }
+      } else {
+        takipciler.remove(currentUid);
+      }
+      takipciCounter.value = nowFollowing
+          ? takipciCounter.value + 1
+          : (takipciCounter.value - 1).clamp(0, 1 << 30);
+      _saveRelationListCache(isFollowers: true);
+      _relationIdSetCache['followers'] = _RelationIdSetCacheEntry(
+        ids: takipciler.toSet(),
+        cachedAt: DateTime.now(),
       );
     }
   }
