7425f7412a90e3086da4c9fd9885d46d6e422ac5
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 08:37:36 +0300

    Merge saved practice exams content into main view

--- lib/Modules/Education/PracticeExams/SavedPracticeExams/saved_practice_exams.dart
+++ lib/Modules/Education/PracticeExams/SavedPracticeExams/saved_practice_exams.dart
@@ -5,8 +5,6 @@ import 'package:turqappv2/Core/Buttons/back_buttons.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/DenemeGrid/deneme_grid.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/SavedPracticeExams/saved_practice_exams_controller.dart';
 
-part 'saved_practice_exams_content_part.dart';
-
 class SavedPracticeExams extends StatefulWidget {
   const SavedPracticeExams({super.key});
 
@@ -50,5 +48,45 @@ class _SavedPracticeExamsState extends State<SavedPracticeExams> {
         ],
       ),
     );
+  }
+
+  Widget _buildSavedPracticeExamsContent() {
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return const Center(child: CupertinoActivityIndicator());
+      }
+
+      if (controller.savedExams.isEmpty) {
+        return Center(
+          child: Text(
+            'practice.saved_empty'.tr,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+              fontFamily: "MontserratMedium",
+            ),
+          ),
+        );
+      }
+
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: GridView.builder(
+          itemCount: controller.savedExams.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 0.52,
+          ),
+          itemBuilder: (context, index) {
+            return DenemeGrid(
+              model: controller.savedExams[index],
+              getData: controller.loadSavedExams,
+            );
+          },
+        ),
+      );
+    });
   }
 }
