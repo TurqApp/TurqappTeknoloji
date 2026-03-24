d1e4676aab6f6c599305b2fc21385fb24c1265f3
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 09:14:43 +0300

    Merge exam results content into main view

--- lib/Modules/Education/PracticeExams/SinavSonuclarim/sinav_sonuclarim.dart
+++ lib/Modules/Education/PracticeExams/SinavSonuclarim/sinav_sonuclarim.dart
@@ -5,8 +5,6 @@ import 'package:turqappv2/Core/Buttons/back_buttons.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/DenemeGecmisSonucContent/deneme_gecmis_sonuc_content.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/SinavSonuclarim/sinav_sonuclarim_controller.dart';
 
-part 'sinav_sonuclarim_content_part.dart';
-
 class SinavSonuclarim extends StatefulWidget {
   const SinavSonuclarim({super.key});
 
@@ -50,5 +48,72 @@ class _SinavSonuclarimState extends State<SinavSonuclarim> {
         ],
       ),
     );
+  }
+
+  Widget _buildSinavSonuclarimContent() {
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return const Center(
+          child: CupertinoActivityIndicator(radius: 20),
+        );
+      }
+
+      if (controller.list.isEmpty) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.lightbulb_outline,
+                  size: 60,
+                  color: Colors.grey,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'practice.results_empty_title'.tr,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: "MontserratBold",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'practice.results_empty_body'.tr,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "MontserratMedium",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+
+      return RefreshIndicator(
+        color: Colors.white,
+        backgroundColor: Colors.black,
+        onRefresh: controller.findAndGetSinavlar,
+        child: Container(
+          color: Colors.white,
+          child: ListView.builder(
+            controller: controller.scrollController,
+            itemCount: controller.list.length,
+            itemBuilder: (context, index) {
+              return DenemeGecmisSonucContent(
+                index: index,
+                model: controller.list[index],
+              );
+            },
+          ),
+        ),
+      );
+    });
   }
 }
