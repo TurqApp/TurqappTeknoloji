a3ca008c7b515e76bef7fbb34b6bbd2d3739265a
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 05:53:55 +0300

    Merge about profile content into main view

--- lib/Modules/Profile/AboutProfile/about_profile.dart
+++ lib/Modules/Profile/AboutProfile/about_profile.dart
@@ -7,8 +7,6 @@ import 'package:turqappv2/Core/functions.dart';
 import 'package:turqappv2/Core/rozet_content.dart';
 import 'package:turqappv2/Modules/Profile/AboutProfile/about_profile_controller.dart';
 
-part 'about_profile_content_part.dart';
-
 class AboutProfile extends StatefulWidget {
   final String userID;
   const AboutProfile({super.key, required this.userID});
@@ -63,6 +61,115 @@ class _AboutProfileState extends State<AboutProfile> {
         bottom: false,
         child: Obx(() => _buildAboutProfileContent()),
       ),
+    );
+  }
+
+  Widget _buildAboutProfileContent() {
+    return Column(
+      children: [
+        BackButtons(text: "about_profile.title".tr),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                ClipOval(
+                  child: SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: controller.avatarUrl.value != ""
+                        ? CachedNetworkImage(
+                            imageUrl: controller.avatarUrl.value,
+                            fit: BoxFit.cover,
+                          )
+                        : Center(
+                            child: CupertinoActivityIndicator(
+                              color: Colors.grey,
+                            ),
+                          ),
+                  ),
+                ),
+                RozetContent(size: 20, userID: widget.userID)
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          controller.nickname.value,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            fontFamily: "MontserratMedium",
+          ),
+        ),
+        Text(
+          controller.fullName.value,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15,
+            fontFamily: "Montserrat",
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Text(
+            "about_profile.description".tr,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 13,
+              fontFamily: "MontserratMedium",
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            children: [
+              Icon(
+                CupertinoIcons.calendar,
+                size: 25,
+                color: Colors.black,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (controller.createdDate.value != "")
+                      Text(
+                        "about_profile.joined_on".trParams(
+                          <String, String>{
+                            'date': formatTimeStampAyYil(
+                              controller.createdDate.value,
+                            ),
+                          },
+                        ),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontFamily: "MontserratMedium",
+                        ),
+                      )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 
