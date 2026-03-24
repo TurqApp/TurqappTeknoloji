be0e703dd5c2e3394a95e5c77e5474e105016498
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 03:09:21 +0300

    Merge language settings header into list

--- lib/Modules/Profile/Settings/language_settings_view_list_part.dart
+++ lib/Modules/Profile/Settings/language_settings_view_list_part.dart
@@ -21,3 +21,33 @@ extension LanguageSettingsViewListPart on LanguageSettingsView {
     );
   }
 }
+
+class _LanguageHeader extends StatelessWidget {
+  const _LanguageHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'language.subtitle'.tr,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontFamily: "MontserratBold",
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          'language.note'.tr,
+          style: const TextStyle(
+            color: Colors.black54,
+            fontSize: 14,
+            fontFamily: "MontserratMedium",
+          ),
+        ),
+      ],
+    );
+  }
+}
