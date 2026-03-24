123706cb00d9f916d41802cd186eec1533a34d3b
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 09:02:57 +0300

    Merge search tests shell into main view

--- lib/Modules/Education/Tests/SearchTests/search_tests.dart
+++ lib/Modules/Education/Tests/SearchTests/search_tests.dart
@@ -6,8 +6,6 @@ import 'package:turqappv2/Modules/Education/Tests/SearchTests/search_tests_contr
 import 'package:turqappv2/Modules/Education/Tests/TestsGrid/tests_grid.dart';
 import 'package:turqappv2/Themes/app_icons.dart';
 
-part 'search_tests_shell_part.dart';
-
 class SearchTests extends StatefulWidget {
   const SearchTests({super.key});
 
@@ -44,6 +42,73 @@ class _SearchTestsState extends State<SearchTests> {
   @override
   Widget build(BuildContext context) {
     return _buildPage();
+  }
+
+  Widget _buildPage() {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            BackButtons(text: 'tests.search_title'.tr),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    _buildSearchField(),
+                    _buildGridContent(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSearchField() {
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 15,
+          right: 15,
+          left: 15,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.withValues(alpha: 0.1),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+          child: TextField(
+            cursorColor: Colors.black,
+            controller: controller.searchController,
+            focusNode: controller.focusNode,
+            onChanged: controller.filterSearchResults,
+            decoration: InputDecoration(
+              hintText: 'common.search'.tr,
+              hintStyle: const TextStyle(color: Colors.grey),
+              prefixIcon: Icon(
+                AppIcons.search,
+                color: Colors.pink,
+              ),
+              border: const OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+              fontFamily: 'MontserratMedium',
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _buildGridContent() {
