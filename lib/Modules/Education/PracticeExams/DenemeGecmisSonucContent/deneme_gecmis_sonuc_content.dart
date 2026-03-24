71c18ca23620de69d679b386e0bfe0e55515fa92
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-24 09:33:41 +0300

    Merge past exam result content into main file

--- lib/Modules/Education/PracticeExams/DenemeGecmisSonucContent/deneme_gecmis_sonuc_content.dart
+++ lib/Modules/Education/PracticeExams/DenemeGecmisSonucContent/deneme_gecmis_sonuc_content.dart
@@ -5,9 +5,6 @@ import 'package:get/get.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/sinav_model.dart';
 import 'package:turqappv2/Modules/Education/PracticeExams/SinavSonuclariPreview/sinav_sonuclari_preview.dart';
 
-part 'deneme_gecmis_sonuc_content_body_part.dart';
-part 'deneme_gecmis_sonuc_content_actions_part.dart';
-
 class DenemeGecmisSonucContent extends StatelessWidget {
   final SinavModel model;
   final int index;
@@ -27,6 +24,100 @@ class DenemeGecmisSonucContent extends StatelessWidget {
           child: _buildResultCard(),
         ),
         Divider(color: Colors.grey.withValues(alpha: 0.2)),
+      ],
+    );
+  }
+
+  void _openResultsPreview() {
+    Get.to(() => SinavSonuclariPreview(model: model));
+  }
+
+  Widget _buildResultCard() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withValues(alpha: 0.1),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          children: [
+            _buildCoverImage(),
+            const SizedBox(width: 12),
+            Expanded(child: _buildResultTexts()),
+            const SizedBox(width: 12),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: Colors.pink,
+                  size: 15,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCoverImage() {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(4)),
+      child: SizedBox(
+        width: 78,
+        height: 78,
+        child: CachedNetworkImage(
+          imageUrl: model.cover,
+          fit: BoxFit.cover,
+          placeholder: (context, url) =>
+              const Center(child: CupertinoActivityIndicator()),
+          errorWidget: (context, url, error) => const Icon(
+            Icons.image_not_supported,
+            size: 50,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildResultTexts() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          model.sinavAdi,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            fontFamily: "MontserratBold",
+          ),
+        ),
+        Text(
+          model.sinavTuru,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            color: Colors.pink,
+            fontSize: 15,
+            fontFamily: "MontserratMedium",
+          ),
+        ),
+        Text(
+          'tests.description_test'
+              .trParams({'description': model.sinavAciklama}),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            fontFamily: "MontserratMedium",
+          ),
+        ),
       ],
     );
   }
--- lib/Modules/Education/PracticeExams/DenemeGecmisSonucContent/deneme_gecmis_sonuc_content_actions_part.dart
+++
@@ -1,7 +0,0 @@
-part of 'deneme_gecmis_sonuc_content.dart';
-
-extension DenemeGecmisSonucContentActionsPart on DenemeGecmisSonucContent {
-  void _openResultsPreview() {
-    Get.to(() => SinavSonuclariPreview(model: model));
-  }
-}
