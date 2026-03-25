f16e0f357681b078754b0e5a40a3d8a384229ffd
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-25 08:52:46 +0300

    Split storage budget manager models into separate part

--- lib/Core/Services/PlaybackIntelligence/storage_budget_manager.dart
+++ lib/Core/Services/PlaybackIntelligence/storage_budget_manager.dart
@@ -1,67 +1,6 @@
 import 'package:get/get.dart';
 
-class StorageBudgetProfile {
-  final int planGb;
-  final int totalPlanBytes;
-  final int mediaQuotaBytes;
-  final int imageQuotaBytes;
-  final int metadataQuotaBytes;
-  final int reserveQuotaBytes;
-  final int osSafetyMarginBytes;
-  final int streamCacheSoftStopBytes;
-  final int streamCacheHardStopBytes;
-
-  const StorageBudgetProfile({
-    required this.planGb,
-    required this.totalPlanBytes,
-    required this.mediaQuotaBytes,
-    required this.imageQuotaBytes,
-    required this.metadataQuotaBytes,
-    required this.reserveQuotaBytes,
-    required this.osSafetyMarginBytes,
-    required this.streamCacheSoftStopBytes,
-    required this.streamCacheHardStopBytes,
-  });
-
-  bool get isValid =>
-      streamCacheSoftStopBytes > 0 &&
-      streamCacheHardStopBytes >= streamCacheSoftStopBytes &&
-      totalPlanBytes > 0;
-
-  Map<String, dynamic> toJson() => {
-        'planGb': planGb,
-        'totalPlanBytes': totalPlanBytes,
-        'mediaQuotaBytes': mediaQuotaBytes,
-        'imageQuotaBytes': imageQuotaBytes,
-        'metadataQuotaBytes': metadataQuotaBytes,
-        'reserveQuotaBytes': reserveQuotaBytes,
-        'osSafetyMarginBytes': osSafetyMarginBytes,
-        'streamCacheSoftStopBytes': streamCacheSoftStopBytes,
-        'streamCacheHardStopBytes': streamCacheHardStopBytes,
-      };
-}
-
-class StorageBudgetUsageSnapshot {
-  final StorageBudgetProfile profile;
-  final int streamUsageBytes;
-  final int remainingBeforeSoftStopBytes;
-  final int remainingBeforeHardStopBytes;
-  final double softUsageRatio;
-  final double hardUsageRatio;
-  final bool crossedSoftStop;
-  final bool crossedHardStop;
-
-  const StorageBudgetUsageSnapshot({
-    required this.profile,
-    required this.streamUsageBytes,
-    required this.remainingBeforeSoftStopBytes,
-    required this.remainingBeforeHardStopBytes,
-    required this.softUsageRatio,
-    required this.hardUsageRatio,
-    required this.crossedSoftStop,
-    required this.crossedHardStop,
-  });
-}
+part 'storage_budget_manager_models_part.dart';
 
 class StorageBudgetManager extends GetxService {
   static StorageBudgetManager? maybeFind() {
@@ -249,20 +188,4 @@ class StorageBudgetManager extends GetxService {
         );
     }
   }
-}
-
-class _BudgetTemplate {
-  final int mediaQuotaBytes;
-  final int imageQuotaBytes;
-  final int metadataQuotaBytes;
-  final int reserveQuotaBytes;
-  final int osSafetyMarginBytes;
-
-  const _BudgetTemplate({
-    required this.mediaQuotaBytes,
-    required this.imageQuotaBytes,
-    required this.metadataQuotaBytes,
-    required this.reserveQuotaBytes,
-    required this.osSafetyMarginBytes,
-  });
 }
