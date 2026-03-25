d949f221f0847b75a6d7fcefdd8602cf20386f02
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-25 09:51:58 +0300

    Split post creator models into separate part

--- lib/Modules/PostCreator/post_creator_controller.dart
+++ lib/Modules/PostCreator/post_creator_controller.dart
@@ -49,46 +49,7 @@ part 'post_creator_controller_publish_part.dart';
 part 'post_creator_controller_publish_upload_part.dart';
 part 'post_creator_controller_route_part.dart';
 part 'post_creator_controller_ui_part.dart';
-
-class PreparedPostModel {
-  final String text;
-  final List<Uint8List> images;
-  final List<String> reusedImageUrls;
-  final double reusedImageAspectRatio;
-  final File? video;
-  final String reusedVideoUrl;
-  final String reusedVideoThumbnail;
-  final double reusedVideoAspectRatio;
-  final String videoLookPreset;
-  final String location;
-  final String gif;
-  final Uint8List? customThumbnail;
-  final Map<String, dynamic> poll;
-
-  PreparedPostModel({
-    required this.text,
-    required this.images,
-    required this.reusedImageUrls,
-    required this.reusedImageAspectRatio,
-    required this.video,
-    required this.reusedVideoUrl,
-    required this.reusedVideoThumbnail,
-    required this.reusedVideoAspectRatio,
-    required this.videoLookPreset,
-    required this.location,
-    required this.gif,
-    required this.customThumbnail,
-    required this.poll,
-  });
-
-  Map<String, dynamic> toMap({required String docID}) => {
-        'id': docID,
-        'text': text,
-        'location': location,
-        'gif': gif,
-        'timeStamp': DateTime.now().millisecondsSinceEpoch,
-      };
-}
+part 'post_creator_controller_models_part.dart';
 
 class PostCreatorController extends GetxController with WidgetsBindingObserver {
   static PostCreatorController ensure({bool permanent = false}) {
