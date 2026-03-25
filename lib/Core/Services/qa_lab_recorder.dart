b8975bf359ca06b2fc01ed31c6990d8d29df1fd5
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-25 06:39:12 +0300

    Split QA recorder health diagnostics into separate part

--- lib/Core/Services/qa_lab_recorder.dart
+++ lib/Core/Services/qa_lab_recorder.dart
@@ -35,6 +35,7 @@ part 'qa_lab_recorder_capture_events_part.dart';
 part 'qa_lab_recorder_capture_issue_part.dart';
 part 'qa_lab_recorder_diagnostics_part.dart';
 part 'qa_lab_recorder_diagnostics_surfaces_part.dart';
+part 'qa_lab_recorder_diagnostics_health_part.dart';
 part 'qa_lab_recorder_diagnostics_playback_part.dart';
 part 'qa_lab_recorder_diagnostics_timeline_part.dart';
 part 'qa_lab_recorder_diagnostics_scroll_part.dart';
