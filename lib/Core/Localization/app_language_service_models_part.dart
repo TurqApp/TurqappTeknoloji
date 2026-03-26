e69d4232f322105cd3bc3df8839ec05d5f7a04a6
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-27 02:12:39 +0300

    Reapply language option named constructor

--- lib/Core/Localization/app_language_service_models_part.dart
+++ lib/Core/Localization/app_language_service_models_part.dart
@@ -1,7 +1,11 @@
 part of 'app_language_service.dart';
 
 class AppLanguageOption {
-  const AppLanguageOption(this.code, this.locale, this.nativeLabel);
+  const AppLanguageOption({
+    required this.code,
+    required this.locale,
+    required this.nativeLabel,
+  });
 
   final String code;
   final Locale locale;
