7a4cb5f621fb797a999fca4ee161ab624e3ee5b7
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-27 02:53:27 +0300

    Compact repository libraries and recorder facade

--- lib/Core/Repositories/profile_repository_library.dart
+++ lib/Core/Repositories/profile_repository_library.dart
@@ -4,7 +4,6 @@ import 'package:turqappv2/Core/Services/PlaybackIntelligence/metadata_read_polic
 import 'package:turqappv2/Core/Repositories/post_repository.dart';
 import 'package:turqappv2/Core/Services/profile_posts_cache_service.dart';
 import 'package:turqappv2/Models/posts_model.dart';
-
 part 'profile_repository_cache_part.dart';
 part 'profile_repository_base_part.dart';
 part 'profile_repository_facade_part.dart';
--- lib/Core/Services/qa_lab_recorder_facade_part.dart
+++ lib/Core/Services/qa_lab_recorder_facade_part.dart
@@ -3,11 +3,8 @@ part of 'qa_lab_recorder.dart';
 extension QALabRecorderFacadePart on QALabRecorder {
   void startSession({String trigger = 'manual'}) =>
       _startSessionImpl(trigger: trigger);
-
   Future<void> prepareFreshStart({String trigger = 'launch'}) =>
       _prepareFreshStartImpl(trigger: trigger);
-
   void resetSession() => _resetSessionImpl();
-
   void disposeSession() => _disposeSessionImpl();
 }
