9deab2b84c250b2ecc016390abf9781dd2d9fd50
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-03-27 03:19:59 +0300

    Fold applications facade and market schema base

--- lib/Modules/Education/Scholarships/Applications/applications_controller_runtime_part.dart
+++ lib/Modules/Education/Scholarships/Applications/applications_controller_runtime_part.dart
@@ -1,5 +1,17 @@
 part of 'applications_controller_library.dart';
 
+ApplicationsController ensureApplicationsController(
+  String tag, {
+  bool permanent = false,
+}) =>
+    maybeFindApplicationsController(tag) ??
+    Get.put(ApplicationsController(), tag: tag, permanent: permanent);
+
+ApplicationsController? maybeFindApplicationsController(String tag) =>
+    Get.isRegistered<ApplicationsController>(tag: tag)
+        ? Get.find<ApplicationsController>(tag: tag)
+        : null;
+
 extension ApplicationsControllerRuntimeX on ApplicationsController {
   Future<void> _handleOnInit() async {
     final userID = CurrentUserService.instance.effectiveUserId;
--- lib/Modules/Market/market_schema_service_fields_part.dart
+++ lib/Modules/Market/market_schema_service_fields_part.dart
@@ -1,5 +1,17 @@
 part of 'market_schema_service_library.dart';
 
+abstract class _MarketSchemaServiceBase extends GetxService {
+  final _state = _MarketSchemaServiceState();
+}
+
+class MarketSchemaService extends _MarketSchemaServiceBase {
+  @override
+  void onInit() {
+    super.onInit();
+    SharedPreferences.getInstance().then((prefs) => _prefs = prefs);
+  }
+}
+
 class _MarketSchemaServiceState {
   final schema = <String, dynamic>{}.obs;
   SharedPreferences? prefs;
