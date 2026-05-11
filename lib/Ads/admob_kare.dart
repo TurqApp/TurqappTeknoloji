184ae7a80c60d3b940bb95f0f1bdbb2549a4d3e3
TurqApp <turqapp@TurqApp-Mac-mini-2.local>
2026-05-11 06:29:24 +0300

    Use test ads in debug builds

--- lib/Ads/admob_kare.dart
+++ lib/Ads/admob_kare.dart
@@ -335,7 +335,7 @@ class _AdmobKareState extends State<AdmobKare> {
   }
 
   static String _resolveAdUnitId() {
-    const bool isTestMode = false;
+    const bool isTestMode = kDebugMode;
     final service = ensureAdmobUnitConfigService();
     final availableIds = service.squareAdUnitIdsForCurrentPlatform(
       isTestMode: isTestMode,
@@ -364,7 +364,7 @@ class _AdmobKareState extends State<AdmobKare> {
       error.message.contains('Too many recently failed requests');
 
   static List<String> _squareAdUnitIdsForCurrentPlatform() {
-    const bool isTestMode = false;
+    const bool isTestMode = kDebugMode;
     final service = ensureAdmobUnitConfigService();
     final ids = service.squareAdUnitIdsForCurrentPlatform(
       isTestMode: isTestMode,
